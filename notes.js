2e8bbeca5de2d71032362c0b0842fa4b3fe3d2c6
Piezel <michalradon06@gmail.com>
2025-09-23 16:24:25 +0200

    init

---
+++ notes.js
@@ -0,0 +1,61 @@
+const creationButton = document.querySelector("#create")
+
+const createNote = (left=100,top=100) => {
+    const stickyNote = document.createElement("textarea")
+    let isdown = false
+    let offset = [0,0]
+    let zindex = 0
+
+    stickyNote.style.cssText = `
+        background-color: #feff9c;
+        width: 200px;
+        height: 200px;
+        position: absolute;
+        top: ${top}px;
+        left: ${left}px;
+        border: 1px solid lightgray;
+        overflow: auto;
+    `
+    stickyNote.addEventListener("dblclick",e=>{
+        e.preventDefault()
+        createNote(stickyNote.style.left.split("px")[0],stickyNote.style.top.split("px")[0])
+    })
+    stickyNote.addEventListener("contextmenu",e=>{
+        e.preventDefault()
+    })
+    stickyNote.addEventListener('mousedown',e=>{
+        if(e.button==1) {
+            stickyNote.remove()
+            stickyNote = null
+        }
+        else if(e.button==2) {
+            e.preventDefault()
+            stickyNote.style.zIndex = 100
+            offset = [
+                stickyNote.offsetLeft - e.clientX,
+                stickyNote.offsetTop - e.clientY
+            ]
+            isdown = true
+        }
+    })
+    stickyNote.addEventListener('mouseup',e=>{
+        if(e.button==2) {
+            isdown = false
+            stickyNote.style.zIndex = zindex
+        }
+    })
+    stickyNote.addEventListener("mousemove",e=>{
+        if(isdown) {
+            stickyNote.style.top = (e.clientY+offset[1])+'px'
+            stickyNote.style.left = (e.clientX+offset[0])+'px'
+        }
+    })
+    stickyNote.addEventListener("mouseleave",e=>{
+        isdown = false
+        stickyNote.style.zIndex = zindex
+    })
+
+    document.body.append(stickyNote)
+}
+
+creationButton.addEventListener("click",createNote)
